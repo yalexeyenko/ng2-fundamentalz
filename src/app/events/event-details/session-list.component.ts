2ff699e22209567a41a69a12bd0dad2e75381093
Yevgeniy Alexeyenko <yevgeniy_alexeyenko@epam.com>
2017-03-15 18:21:43 +0600

    79-Sorting Data

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -15,11 +15,14 @@ export class SessionListComponent implements OnChanges {
     sessions: ISession[];
     @Input()
     filterBy: string;
+    @Input()
+    sortBy: string;
     visibleSessions: ISession [] = []
 
     ngOnChanges(): void {
         if (this.sessions) {
             this.filterSessions(this.filterBy)
+            this.sortBy === 'name' ? this.visibleSessions.sort(sortByNameAsc) : this.visibleSessions.sort(sortByVotesDesc);
         }
     }
 
@@ -31,4 +34,15 @@ export class SessionListComponent implements OnChanges {
             this.visibleSessions = this.sessions.filter(session => {return session.level.toLocaleLowerCase() === filter;})
         }
     }
+
+}
+    
+function sortByNameAsc(s1: ISession, s2: ISession) {
+    if (s1.name > s2.name) return 1
+    else if (s1.name === s2.name) return 0
+    else return -1
+}
+
+function sortByVotesDesc(s1: ISession, s2: ISession) {
+    return s2.voters.length - s1.voters.length;
 }
