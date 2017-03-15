f6e0b47ca632fc3acc4b440eb7577ba1d5ec5e4a
Yevgeniy Alexeyenko <yevgeniy_alexeyenko@epam.com>
2017-03-15 17:25:31 +0600

    77-Creating a Filtering Display

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -15,6 +15,7 @@ import { IEvent, ISession } from "../shared/event.model";
 export class EventDetailsComponent implements OnInit {
     event: IEvent
     addMode: boolean
+    filterBy: string = 'all';
 
     constructor(
         private eventService: EventService,
