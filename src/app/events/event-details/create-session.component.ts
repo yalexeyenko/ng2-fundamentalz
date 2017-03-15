611510a0ab35f5e09cd8ae576b0e7f891f6f858a
Yevgeniy Alexeyenko <yevgeniy_alexeyenko@epam.com>
2017-03-15 12:11:03 +0600

    67-Passing Data out of a Child Component

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Output, EventEmitter } from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { ISession } from "../shared/event.model";
 import { restrictedWords } from "../shared/restricted-words.validator";
 
 @Component({
+    selector: 'create-session',
     templateUrl: './create-session.component.html',
     styles: [`
     em {float: right; color: #e05c65; padding-left: 10px;}
@@ -16,6 +17,10 @@ import { restrictedWords } from "../shared/restricted-words.validator";
 })
 
 export class CreateSessionComponent implements OnInit {
+    @Output()
+    saveNewSession = new EventEmitter()
+    @Output()
+    cancelAddSession = new EventEmitter()
     newSessionForm: FormGroup
     name: FormControl
     presenter: FormControl
@@ -50,6 +55,10 @@ export class CreateSessionComponent implements OnInit {
             abstract: formValues.abstract,
             voters: []
         }
-        console.log(session)
+        this.saveNewSession.emit(session)
+    }
+
+    cancel() {
+        this.cancelAddSession.emit()
     }
 }
